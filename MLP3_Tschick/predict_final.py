30af3d1b3853cedfe065ad935fdd045fcc8eaab0
Caifa Zhou <czhou@phys.ethz.ch>
2017-01-01 19:20:37 +0100

    updates

--- MLP3_Tschick/predict_final.py
+++ MLP3_Tschick/predict_final.py
@@ -20,12 +20,11 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.pipeline import make_pipeline
-#from src.fMRIToFeature import *
+from src.fMRIToFeature import *
 
 #Here we load the Data.
-#@Caifa: This will probably not work on your computer as I am still on an old python version. Just change the function
 # mriToHistFeature in src.fMRIToFeature to your importing function and it will work
-'''bins = 64
+bins = 64
 block = 8
 testDir = './data/set_test/'
 trainDir = './data/set_train/'
@@ -33,10 +32,9 @@ trainDir = './data/set_train/'
 featureTest = mriToHistFeature(testDir, bins, block)
 csvOutput('./src/testFeatureHistR.csv', featureTest)
 featureTrain = mriToHistFeature(trainDir, bins, block)
-csvOutput('./src/trainFeatureHistR.csv', featureTrain)'''
+csvOutput('./src/trainFeatureHistR.csv', featureTrain)
 
 #Here we start the logistic regression.
-
 #load the data using the function loadData in src.functions
 trainingTargets, trainingFeatures, testingFeatures = loadData()
 
@@ -52,12 +50,7 @@ classif = OneVsRestClassifier(LogisticRegressionCV(Cs=10, fit_intercept=True, cv
 pipe = make_pipeline(StandardScaler(), classif)
 pipe.fit(X, Y)
 
-# anss = pipe.predict(Z)
-# csvMLP3FormatedOutput('final_sub.csv', anss)
+anss = pipe.predict(Z)
+csvMLP3FormatedOutput('final_sub.csv', anss)
 
-# ada
-nClf = trainingTargets.shape[1]
-nBoost = 600
-boostEps, boostAlp, boostModel = adaMultiOutBoost(pipe, nBoost, trainingFeatures, trainingTargets)
-adaPredicted = adaBoostMultiPredict(boostModel, boostAlp, nBoost, testingFeatures, nClf)
-csvMLP3FormatedOutput('../OvRAdaLogHistR600.csv', adaPredicted)
+
